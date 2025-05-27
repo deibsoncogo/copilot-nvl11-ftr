c738eb390fb2e96e41198771847362ac60f9251d
Deibson Cogo <deibsoncogo@outlook.com>
2025-05-26 21:31:46 -0300

    feat: adiciona rotas de demonstração para testes de erro

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bug-simples', function () {
+    throw new Exception('Erro de demonstração do Sentry nume rota GET');
+});
+
+Route::get('/bug-array', function () {
+    $array = ['name' => 'João'];
+    return $array['email'];
+});
+
+Route::get('/bug-loop', function () {
+    while (true) { }
+});
+
+Route::get('/bug-tipo', function () {
+    function sum(int $a, int $b) {
+        return $a + $b;
+    }
+
+    return sum('um', 2);
+});
